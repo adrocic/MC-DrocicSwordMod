4c7d81ba5ac25fcc1029842039e02b7649c96f91
Anel Drocic <anel.drocic@gmail.com>
2023-04-08 16:28:41 -0500

    Changed naming
    Signed-off-by: Anel Drocic <anel.drocic@gmail.com>

--- src/main/java/com/drocic/drocicswordmod/CommonProxy.java
+++ src/main/java/com/drocic/drocicswordmod/CommonProxy.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.drocic.drocicswordmod;
 
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.player.EntityPlayer;
@@ -11,29 +11,31 @@ import net.minecraft.item.Item;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import java.util.Objects;
+
 @Mod.EventBusSubscriber(modid = "sword") // this subscribes this class to the event bus during loading of the game to register our sword
 public class CommonProxy {
     // now we need to actualy write the code that will do that
     private static boolean itemAdded = false;
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event) {
-        event.getRegistry().registerAll(ExampleMod.mySword);
+        event.getRegistry().registerAll(DrocicSwordMod.mySword);
     }
 
     public static void registerRender(Item item) {
-        ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation( item.getRegistryName(), "inventory"));
+        ModelLoader.setCustomModelResourceLocation(item, 0, new ModelResourceLocation(Objects.requireNonNull(item.getRegistryName()), "inventory"));
     }
 
     @SubscribeEvent
     public static void registerRenders(ModelRegistryEvent event) {
-        registerRender(ExampleMod.mySword);
+        registerRender(DrocicSwordMod.mySword);
     }
 
     @SubscribeEvent
     public static void onPlayerUpdate(LivingEvent.LivingUpdateEvent event) {
         if (!itemAdded && event.getEntity() instanceof EntityPlayer) {
             EntityPlayer player = (EntityPlayer) event.getEntity();
-            ItemStack itemStack = new ItemStack(ExampleMod.mySword, 2); // Give 2 of the item
+            ItemStack itemStack = new ItemStack(DrocicSwordMod.mySword, 2); // Give 2 of the item
             try {
                 player.inventory.addItemStackToInventory(itemStack);
                 System.out.println("DrocicSwordMod: added item");
--- src/main/java/com/drocic/drocicswordmod/CustomSword.java
+++ src/main/java/com/drocic/drocicswordmod/CustomSword.java
@@ -1,11 +1,11 @@
-package com.example.examplemod;
+package com.drocic.drocicswordmod;
 
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemSword;
 
 public class CustomSword extends ItemSword {
     public CustomSword() {
-        super(ExampleMod.myToolmaterial);
+        super(DrocicSwordMod.myToolmaterial);
         this.setRegistryName("my_sword");
         this.setUnlocalizedName("my_sword");
         this.setCreativeTab(CreativeTabs.COMBAT);
